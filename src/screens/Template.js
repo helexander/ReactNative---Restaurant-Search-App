b9980f7ea48c7c1cc9457785ef2112b4040ad36f
helexander <jeremychee97@gmail.com>
2021-08-03 11:51:53 +1000

    Complete search bar component

---
+++ src/screens/Template.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const _ = () => {
+    return (
+        <View>
+            <Text>_ Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default _;
